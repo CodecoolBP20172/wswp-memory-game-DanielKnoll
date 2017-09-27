37789035245455d83649e5590655edc3fc363c3b
Dani <knoll.dani@gmail.com>
2017-09-27 22:01:46 +0200

    Skeleton added.

---
+++ server.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, redirect, request, url_for
+from functools import wraps
+import werkzeug.exceptions
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index_page():
+    return render_template('index.html')
+
+
+@app.route('/game')
+def index_page():
+    return render_template('game.html')
+
+
+if __name__ == "__main__":
+    app.secret_key = 'magic'
+    app.run(debug=True, port=5000)
